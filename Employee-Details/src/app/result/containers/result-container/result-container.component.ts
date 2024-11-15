37d16af4201bac98c4f60ce8d339171bf3806034
Lalit Shrawansing Rajput <67156745+lalit-s-rajput@users.noreply.github.com>
2024-11-15 22:06:09 +0530

    feat: filter view added (#3)

--- Employee-Details/src/app/result/containers/result-container/result-container.component.ts
+++ Employee-Details/src/app/result/containers/result-container/result-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ResultsService } from '../../services/results.service';
 import { BehaviorSubject } from 'rxjs';
 import { ResultData } from 'src/app/core/interface';
@@ -8,11 +8,13 @@ import { ResultData } from 'src/app/core/interface';
   templateUrl: './result-container.component.html',
   styleUrls: ['./result-container.component.scss'],
 })
-export class ResultContainerComponent implements OnInit {
-  resultData$ = this.resultService.resultsSubject$;
+export class ResultContainerComponent implements OnInit, OnDestroy {
+  _resultData$ = this.resultService.resultsSubjectToShow$;
+  _designationData = this.resultService.designationsData$;
   constructor(private resultService: ResultsService) {}
   ngOnInit(): void {
     this.resultService.getData();
+    //this._designationData = this.resultService.getAllDesignations();
   }
   deleteEmp(id: string) {
     console.log(id);
@@ -21,5 +23,17 @@ export class ResultContainerComponent implements OnInit {
   editEmpData(data: ResultData) {
     console.log('editedData', data);
     this.resultService.editEmployee(data);
+  }
+  searchFilter(key: string) {
+    this.resultService.filterOnSearch(key);
+  }
+  designationFilter(designation: string) {
+    this.resultService.designationFilter(designation);
+  }
+  resetAll() {
+    this.resultService.resetAllFilters();
+  }
+  ngOnDestroy(): void {
+    this._resultData$.unsubscribe();
   }
 }
--- Employee-Details/src/app/result/services/results.service.ts
+++ Employee-Details/src/app/result/services/results.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { Data, ResultData } from 'src/app/core/interface';
+import { Data, filterState, ResultData } from 'src/app/core/interface';
 @Injectable({
   providedIn: 'root',
 })
 export class ResultsService {
   private getAllData: string = 'http://localhost:3000/data';
-  // private deleteEmp:string = "http://localhost:3000/data";
   resultsSubject$: BehaviorSubject<ResultData[]> = new BehaviorSubject<
     ResultData[]
   >([]);
+  resultsSubjectToShow$: BehaviorSubject<ResultData[]> = new BehaviorSubject<
+    ResultData[]
+  >([]);
+  designationsData$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
+    []
+  );
+  filterObject: filterState = { searchString: '', designation: '' };
   constructor(private http: HttpClient) {}
   getData() {
     this.http.get<ResultData[]>(this.getAllData).subscribe({
       next: (resultData: ResultData[]) => {
-        this.resultsSubject$.next(resultData);
+        this.resultsSubject$.next([...resultData]);
+        this.setAllDesignations();
+        this.resultsSubjectToShow$.next([...resultData]);
       },
       error: (error) => {
         console.log(error);
@@ -45,5 +53,36 @@ export class ResultsService {
           console.log(error);
         },
       });
+  }
+  filterOnSearch(key: string) {
+    let keyData = key.toLowerCase();
+    this.filterObject.searchString = keyData;
+    let data = this.resultsSubjectToShow$.value.filter((item) => {
+      return (
+        item.empEmailId.toLowerCase().includes(keyData) ||
+        item.empName.toLowerCase().includes(keyData)
+      );
+    });
+    this.resultsSubjectToShow$.next(data);
+  }
+  designationFilter(designation: string) {
+    this.filterObject.designation = designation;
+    let data = this.resultsSubjectToShow$.value.filter((item) => {
+      return item.empDesignation === designation;
+    });
+    this.resultsSubjectToShow$.next(data);
+  }
+  setAllDesignations() {
+    let data = this.resultsSubject$.value.reduce(
+      (acc: string[], curr: ResultData) => {
+        acc.push(curr.empDesignation);
+        return acc;
+      },
+      []
+    );
+    this.designationsData$.next(data);
+  }
+  resetAllFilters() {
+    this.resultsSubjectToShow$.next([...this.resultsSubject$.value]);
   }
 }
